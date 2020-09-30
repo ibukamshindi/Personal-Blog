8debb03711d682e3e5a7515f1a5e8e0bfbf8c802
ibukamshindi <ibukamshindi@gmail.com>
2020-09-30 22:05:34 +0300

    set os.env variables

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:201400@localhost/blog'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:201400@localhost/blog'
     SECRET_KEY = 'huwezi sahau'
 
 class ProdConfig(Config):
@@ -11,8 +11,10 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:201400@localhost/blog'
     DEBUG = True
 
 config_options = {
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export SECRET_KEY='huwezi sahau'
+export DATABASE_URL='postgresql+psycopg2://patrick:201400@localhost/blog'
 
 python3.8 manage.py server
